dc554cbd0fb4a4ef8b66ff9c02be4156baba62d6
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2014-10-10 09:07:56 -0700

    Adds icons

--- functions.php
+++ functions.php
@@ -4,9 +4,8 @@ include_once( __DIR__ . '/includes/cereo-people.php' );
 
 add_action( 'wp_footer', 'cereo_accordion_script' );
 function cereo_accordion_script() {
-	?><script>(function($) { $( "#accordion" ).accordion({ collapsible: true, heightStyle: "content" }); }(jQuery));</script><?php
+	?><script>(function($) { $( "#accordion" ).accordion({ collapsible: true, heightStyle: "content", icons:true }); }(jQuery));</script><?php
 }
-
 add_action( 'pre_get_posts', 'cereo_display_members_query' );
 /**
  * @param WP_Query $query
