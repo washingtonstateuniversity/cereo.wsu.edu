3932bbc5ac10a54baec0515e909867a13e52f9d4
jeremyfelt <jeremy.felt@gmail.com>
2014-10-10 08:42:20 -0700

    Move accordion script to wp_footer action

--- functions.php
+++ functions.php
@@ -1,3 +1,12 @@
 <?php
 
 include_once( __DIR__ . '/includes/cereo-people.php' );
+
+add_action( 'wp_footer', 'cereo_accordion_script' );
+function cereo_accordion_script() {
+	?><script>$(function() { $( "#accordion" ).accordion({
+      collapsible: true
+    });
+  });
+  </script><?php
+}
