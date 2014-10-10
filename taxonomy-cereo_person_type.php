e673596a3261ae1fce256fba7f34d2591c36112b
jeremyfelt <jeremy.felt@gmail.com>
2014-10-10 09:00:52 -0700

    Update person type archive template

--- taxonomy-cereo_person_type.php
+++ taxonomy-cereo_person_type.php
@@ -21,28 +21,7 @@ get_header();
 			</div><!--/column-->
 
 		</section>
-		<?php
-		/* @type WP_Query $wp_query */
-		global $wp_query;
 
-		$big = 99164;
-		$args = array(
-			'base'         => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-			'format'       => 'page/%#%',
-			'total'        => $wp_query->max_num_pages, // Provide the number of pages this query expects to fill.
-			'current'      => max( 1, get_query_var('paged') ), // Provide either 1 or the page number we're on.
-		);
-		?>
-		<footer class="main-footer archive-footer">
-			<section class="row side-right pager prevnext gutter">
-				<div class="column one">
-					<?php echo paginate_links( $args ); ?>
-				</div>
-				<div class="column two">
-					<!-- intentionally empty -->
-				</div>
-			</section><!--pager-->
-		</footer>
 	</main>
 <?php
 
