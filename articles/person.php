4a0a2e1b7516ff417402a455e667a370757bf950
jeremyfelt <jeremy.felt@gmail.com>
2014-10-10 15:46:06 -0700

    Style affiliate output for accordion to work

--- articles/person.php
+++ articles/person.php
@@ -1,13 +1,8 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-	<header class="article-header">
-		<hgroup>
-			<h1 class="article-title"><?php the_title(); ?></h1>
-		</hgroup>
-	</header>
-
-	<div class="article-body">
-		<?php the_content(); ?>
-	</div>
-
-</article>
+<?php if ( $last_name = get_post_meta( get_the_ID(), 'cereo_person_last_name', true ) ) : ?>
+	<h2 class="article-title person-last-name"><?php echo esc_html( $last_name ); ?></h2>
+<?php else : ?>
+	<h2 class="article-title person-last-name"><?php the_title(); ?></h2>
+<?php endif; ?>
+<div class="article-body">
+	<?php the_content(); ?>
+</div>
--- taxonomy-cereo_person_type.php
+++ taxonomy-cereo_person_type.php
@@ -3,14 +3,14 @@
 get_header();
 
 ?>
-
+   <style>.person-last-name { font-size: 1rem; }</style>
 	<main class="spine-tax-index">
 
 		<?php get_template_part('parts/headers'); ?>
 
 		<section class="row single gutter pad-ends">
 
-			<div class="column one">
+			<div class="column one" id="accordion">
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
