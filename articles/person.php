b013365934161622c8bc8b9bc9fab98b3b254560
jeremyfelt <jeremy.felt@gmail.com>
2014-11-10 10:21:48 -0800

    Update person display to show dept and theme in preview

--- articles/person.php
+++ articles/person.php
@@ -1,8 +1,14 @@
-<?php if ( $last_name = get_post_meta( get_the_ID(), 'cereo_person_last_name', true ) ) : ?>
-	<h2 class="article-title person-last-name"><?php echo esc_html( $last_name ); ?></h2>
-<?php else : ?>
+<div class="article-title">
 	<h2 class="article-title person-last-name"><?php the_title(); ?></h2>
-<?php endif; ?>
+	<ul class="person-short-info">
+		<?php if ( $cereo_person_dept = get_post_meta( get_the_ID(), 'cereo_person_department', true ) ) : ?>
+			<li>Department: <?php echo esc_html( $cereo_person_dept ); ?></li>
+		<?php endif; ?>
+		<?php if ( $cereo_person_theme = get_post_meta( get_the_ID(), 'cereo_person_theme', true ) ) : ?>
+			<li>CEREO Theme: <?php echo esc_html( $cereo_person_theme ); ?></li>
+		<?php endif; ?>
+	</ul>
+</div>
 <div class="article-body">
 	<?php the_content(); ?>
 </div>
